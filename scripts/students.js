c16814228073bdc92235c21a6fd42d454092598d
Arnout <arnout.hennion@student.arteveldehs.be>
2024-02-14 11:21:19 +0100

    Arnout

--- scripts/students.js
+++ scripts/students.js
@@ -2,4 +2,5 @@ const students = [
     { name: 'Dieter De Weirdt', github: 'https://dieterdeweirdt.github.io' },
     { name: 'Olivier Vandenberghe', github: 'https://OlivierVDB2002.github.io' },
     { name: 'Jonah De Smet', github: 'https://janoahdev.github.io' },
+    { name: 'Arnout Hennion', github: 'https://djkaas.github.io/' },
 ];
