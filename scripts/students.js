99dc0ed7b8df16a9bd4392682d416fcf282cb67a
Olivier Vandenberghe <olivier.vandenberghe@student.arteveldehs.be>
2024-02-14 11:18:07 +0100

    Upload Olivier V

--- scripts/students.js
+++ scripts/students.js
@@ -1,3 +1,4 @@
 const students = [
     { name: 'Dieter De Weirdt', github: 'https://dieterdeweirdt.github.io' },
+    { name: 'Olivier Vandenberghe', github: 'https://OlivierVDB2002.github.io' },
 ];
